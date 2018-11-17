e37596f752425a29055ac1fa5cea3c250e846be4
MagicSHX <34002636+MagicSHX@users.noreply.github.com>
2018-11-17 11:04:14 +0800

    y

--- src/PID.h
+++ src/PID.h
@@ -1,121 +1,62 @@
-/*
- *  PID.cpp
- *  purpose: Uses a PID to control steering and throttle response
- *
- *  @author Bruno Guisard
- *  @version 0.1 6/23/2017
- */
-
 #ifndef PID_H
 #define PID_H
 
-#include <uWS/uWS.h>
-
-using namespace std::chrono;
-
 class PID {
 public:
- /*
+  /*
   * Errors
   */
   double p_error;
   double i_error;
   double d_error;
-  double prev_cte;
-  double e[3];
-  double twiddle_error;
 
- /*
+  /*
   * Coefficients
   */ 
   double Kp;
   double Ki;
   double Kd;
-  double p[3];
 
- /*
-  * Optimizer Coefficients
+  /*
+  * Twiddle
   */
-  // constant integers control the main characteristics of the optimizer
-  const int start_meas = 300;         // starts measuring error for twiddle
-  const int end_meas = 700;           // ending measuring error for twiddle  
-  const double twiddle_w_tol = 0.2;   // Twiddle weights tolerance
-  const int max_iters = 5000;           // Maximum number of iterations
-  const int num_params = 3;
+  float tolerance;
+  double delta_p;
 
-  double dp[3];
-  double best_p[3];
-  double best_err;
-
-  int optimizer_t;
-  int twiddle_pass;
-  int twiddle_iter;
-
-  bool use_twiddle = false;
-  bool is_retrial = false;
-
-  // server
-  uWS::WebSocket<uWS::SERVER> server;
-
- /*
+  /*
   * Constructor
   */
   PID();
 
-/*
+  /*
   * Destructor.
   */
   virtual ~PID();
 
- /*
-  * Initializes PID.
+  /*
+  * Initialize PID.
   */
-  void Init(double K_p, double K_i, double K_d);
+  void Init(double Kp, double Ki, double Kd, bool run_twiddle);
 
-/*
-  * Updates the PID error variables given cross track error.
+  /*
+  * Update the PID error variables given cross track error.
   */
   void UpdateError(double cte);
 
- /*
-  * Calculates the total PID error.
+  /*
+  * Calculate the total PID error.
   */
   double TotalError();
 
- /*
-  * Calculates the steering angle.
+  /*
+  * Used to reset the simulator and bring the car back to first position.
   */
-  double CalculateOutput();
+  void Restart(uWS::WebSocket<uWS::SERVER> ws);
 
- /*
-  * Calculates the throttle response
-  * if we had data to measure the target speed at any point
-  * this could be implemented as another PID
-  * but because we don't I used a throttle that is
-  * inversely proportional to steering angle and speed.
+  /*
+  * Twiddle or vanilla gradient descent for tuning one hyper parameter at a time
   */
-  double CalculateThrottle(double speed, double steering_angle);
-
- /*
-  * Initializes twiddle parameters.
-  */
-  void InitializeTwiddle();
-
- /*
-  * Optimizes tau values (Kp, Ki, Kd).
-  */
-  void Twiddle();
-
- /*
-  * Restarts simulator.
-  */
-  void RestartSim(uWS::WebSocket<uWS::SERVER> ws);
-
- /*
-  * Stores server info.
-  */
-  void SetServer(uWS::WebSocket<uWS::SERVER> ws);
+  void Twiddle(double total_error, double hyperparameter);
 };
-
 
 #endif /* PID_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <uWS/uWS.h>
 #include <iostream>
+#include <string>
 #include "json.hpp"
 #include "PID.h"
 #include <math.h>
 
 // for convenience
 using json = nlohmann::json;
+
+//Global variable
+bool run_twiddle = false;
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
@@ -28,24 +32,39 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+int main(int argC, char** argV)
 {
   uWS::Hub h;
 
   PID pid;
-  // works at 30mph
-  double K_p = 0.29282; 
-  double K_i = 0.0058564;
-  double K_d = 6.84567;
 
-  pid.Init(K_p, K_i, K_d);
-  //pid.InitializeTwiddle();
+  double Kp_initial, Ki_initial, Kd_initial;
 
+  if (argC > 1) {
+    Kp_initial = atof(argV[1]);
+    Ki_initial = atof(argV[2]);
+    Kd_initial = atof(argV[3]);
+    if (argC > 4) {
+      std::string is_run_twiddle = argV[4];
+      if (is_run_twiddle.compare("twiddle") == 0) {
+        run_twiddle = true;
+      }
+    }
+  } else {
+    Kp_initial = -0.091;
+    Ki_initial = -0.0005;
+    Kd_initial = -1.693;
+  }
+  //Init PID with P, I and D constants
+  std::cout<<"Kp is: "<<Kp_initial<<" Ki is: "<<Ki_initial<<" Kd is: "<<Kd_initial<<std::endl;
+  pid.Init(Kp_initial, Ki_initial, Kd_initial, run_twiddle);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+    static unsigned int timesteps = 0;
+    static double total_error = 0.0;
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
       auto s = hasData(std::string(data).substr(0, length));
@@ -58,22 +77,30 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
-          double throttle;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          pid.Twiddle();
+          
+          //Update the error in system using cte.
           pid.UpdateError(cte);
-          steer_value = pid.CalculateOutput();
-          throttle = pid.CalculateThrottle(speed, angle);
-
-
+          //Derive the steering angle in order to minimize the error (or cte).
+          steer_value = pid.TotalError();
+          
+          // DEBUG
+          //std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          //std::cout << "Timestep: " << timesteps << std::endl;
+          if (run_twiddle) {
+            if (timesteps > 500) {
+              pid.Twiddle(total_error, pid.Kp);
+              pid.Restart(ws);
+              timesteps = 0;
+              total_error = 0.0;
+              return;
+            } else {
+              total_error += pow(cte, 2);
+            }
+            timesteps++;
+          }
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle;
+          msgJson["throttle"] = 0.4;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
@@ -101,9 +128,8 @@ int main()
     }
   });
 
-  h.onConnection([&h, &pid](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
     std::cout << "Connected!!!" << std::endl;
-    pid.SetServer(ws);
   });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
